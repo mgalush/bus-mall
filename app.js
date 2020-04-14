c0ad874d9b9c75a538d6d851beab8c81379f9d25
Melissa Galush <galushmn@gmail.com>
2020-04-13 18:15:01 -0700

    moved images into divs

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 'use strict';
 // 1: randomly display three unique products
+// 2: track selections made by viewers
+// 3: control the number of rounds a user is presented with so that I can control the voting session duration
+
 
 // create constructor function that creates an object associated with each product and has the following properties:
 //           name of product
 //           file path of image
+// in constructor function, define a property to hold the number of times a product has been clicked
+// after every selection by the viewer, update the newly added property to reflect if it was clicked
 
 let allProducts = [];
 
@@ -15,29 +20,32 @@ function Product(name, imageSrc) {
   allProducts.push(this);
 }
 
-new Product('bag', 'img/bag.jpg');
-new Product('banana', 'img/banana.jpg');
-new Product('bathroom', 'img/bathroom.jpg');
-new Product('boots', 'img/boots.jpg');
-new Product('breakfast', 'img/breakfast.jpg');
-
 // render product images to page
-// section class="images"
+
+///// div.classList.add("anotherclass");
+
 
 Product.prototype.render = function () {
-  let targetProduct = document.getElementById('images');
+  let target = document.getElementById('productContainer');
+  let div = document.createElement('div');
   let newImg = document.createElement('img');
-
+  newImg.classList.add('product');
+  
   // add event listener to product images
   newImg.addEventListener('click', (evento) => {
-    console.log(this.clickCount);
     this.clickCount++;
+    console.log(this.clickCount);
   });
-
+  
   newImg.src = this.imageSrc;
   newImg.name = this.name;
-
-  targetProduct.appendChild(newImg);
+  
+  let newCount = document.createElement('p');
+  newCount.innerText = 'Score: ' + this.clickCount;
+  
+  target.appendChild(div);
+  div.appendChild(newImg);
+  div.appendChild(newCount);
 };
 
 // TODO: remove duplicates from list
@@ -46,38 +54,35 @@ Product.prototype.render = function () {
 
 function randomlyGenerateProducts() {
   let randomlyGeneratedProduct1 =
-    allProducts[Math.floor(Math.random() * allProducts.length)];
-
+  allProducts[Math.floor(Math.random() * allProducts.length)];
+  
   let randomlyGeneratedProduct2 =
-    allProducts[Math.floor(Math.random() * allProducts.length)];
-
+  allProducts[Math.floor(Math.random() * allProducts.length)];
+  
   let randomlyGeneratedProduct3 =
-    allProducts[Math.floor(Math.random() * allProducts.length)];
-
+  allProducts[Math.floor(Math.random() * allProducts.length)];
+  
   randomlyGeneratedProduct1.render();
   randomlyGeneratedProduct2.render();
   randomlyGeneratedProduct3.render();
 }
-randomlyGenerateProducts();
 
 // once the user clicks a product, generate three new products for the user to choose from
 
-let targetProduct = document.getElementById('images');
+let targetProduct = document.getElementById('productContainer');
 targetProduct.addEventListener('click', (event) => {
   targetProduct.innerHTML = '';
   randomlyGenerateProducts();
 });
 
 
-// 2: track selections made by viewers
 
-// in constructor function, define a property to hold the number of times a product has been clicked
-// after every selection by the viewer, update the newly added property to reflect if it was clicked
-
-// 3: control the number of rounds a user is presented with so that I can control the voting session duration
 
 // by default, show user 25 rounds of voting before ending session
 // store number of rounds in varaible
+
+
+
 
 // 4: show report of results after each round has concluded
 
@@ -85,3 +90,18 @@ targetProduct.addEventListener('click', (event) => {
 // after voting has concluded, remove the event listeners from the product
 // display list of all products followed by votes receive and number of times seen
 //      example: Banana Slicer had 3 votes and was shown 5 times
+
+
+
+
+
+
+new Product('bag', 'img/bag.jpg');
+new Product('banana', 'img/banana.jpg');
+new Product('bathroom', 'img/bathroom.jpg');
+new Product('boots', 'img/boots.jpg');
+new Product('breakfast', 'img/breakfast.jpg');
+
+
+
+randomlyGenerateProducts();
